6ecd3404905198e060a76b25dbea405c943b3ba7
mistryharsh1 <mistryharsh06@gmail.com>
2024-02-13 14:28:05 +0530

    Cleanup and components added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to my website
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Hello World</h1>
+     <Header/>
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import '../App.css';
+// import App from '../App';
+
+function Header() {
+  return (
+    <div className="Header">
+     <h1>This is Header Component</h1>
+     {/* <App/> */}
+    </div>
+  );
+}
+
+export default Header;
